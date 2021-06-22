cf4060043595f38c770f78e9e8c77b3e80f45fd6
Luca Dorigo <dorigoluca@gmail.com>
2021-06-22 16:52:27 +0200

    Changed test structure and added a few testcases

--- test.py
+++ test.py
@@ -7,14 +7,29 @@ nlp = spacy.load("en_core_sci_md")
 
 
 class TestParser(unittest.TestCase):
-    def test_passive(self):
-        sent_1 = "By meta-analysis, we found A1166C polymorphism was associated with decreased risk for breast cancer in Caucasian population in an additive model . "
-        expected_result = ["[[A1166C, polymorphism] (~)] <-> [[breast, cancer] (↓)]"]
+    def test_passive_association(self):
+        sents_dict = {
+            "By meta-analysis, we found A1166C polymorphism was associated with decreased risk for breast cancer in Caucasian population in an additive model . ": [
+                "[[A1166C, polymorphism] (~)] <-> [[breast, cancer] (↓)]"
+            ],
+            "The use of HAART was associated with a significant increase in both physical and mental aspects of the HRQOL over a 12-month period in this urban African population.": [
+                "[[HAART] (~)] <-> [[HRQOL] (↑)]"
+            ],
+            "CCL2 responses to Mycobacterium tuberculosis are associated with disease severity in tuberculosis.": [
+                "[[CCL2, responses] (~)] <-> [[tuberculosis] (~)]",
+                "[[CCL2, responses, to, Mycobacterium, tuberculosis] (~)] <-> [[tuberculosis] (~)]",
+            ],
+            "Gestational diabetes mellitus is associated with higher RT during pregnancy compared with non-GDM.": [
+                "[[Gestational, diabetes, mellitus] (~)] <-> [[RT] (↑)]",
+                "[[diabetes, mellitus] (~)] <-> [[RT] (↑)]",
+            ],
+        }
+        for sentence in sents_dict:
 
-        ns = nlp(sent_1)
-        relations = parse_sentence(ns, nlp)
-        pp = [pretty_print_relation(rel) for rel in relations]
-        self.assertSequenceEqual(expected_result, pp)
+            ns = nlp(sentence)
+            relations = parse_sentence(ns, nlp)
+            pp = [pretty_print_relation(rel) for rel in relations]
+            self.assertSequenceEqual(sents_dict[sentence], pp)
 
 
 if __name__ == "__main__":
